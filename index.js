77532e6ba52b505b1626d01c4b53f1324f2a2f1a
Tyson Romero <romennoodles23@yahoo.com>
2021-08-20 22:29:27 -0600

    next and prev buttons transitioning from img to img on carousel

--- index.js
+++ index.js
@@ -10,16 +10,28 @@ carouselButtonNext.addEventListener('click', moveToNextSlide)
 carouselButtonPrev.addEventListener('click', moveToPrevSlide)
 
 function moveToNextSlide() {
+    hideAllslides()
     if (slidePosition === totalSlides - 1) {
         slidePosition = 0
     } else {
         slidePosition++
     }
-
     slides[slidePosition].classList.add("carousel-item-visible")
 }
 
 function moveToPrevSlide() {
-    console.log("prev")
+    hideAllslides()
+    if (slidePosition === 0) {
+        slidePosition = totalSlides - 1
+    } else {
+        slidePosition--
+    }
+    slides[slidePosition].classList.add("carousel-item-visible")
 }
 
+function hideAllslides() {
+    for (let slide of slides) {
+        slide.classList.remove("carousel-item-visible")
+        slide.classList.add("carousel-item-hidden")
+    }
+}
